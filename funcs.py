44f9df1a45efa18858571ae552a9d2eb81c9c9dc
PyDevJ <gitcloudhab@gmail.com>
2023-09-17 00:21:12 +0300

    create function

---
+++ funcs.py
@@ -0,0 +1,9 @@
+def folding(num_a, num_b):
+    """Сложение двух чисел"""
+    return num_a + num_b
+
+
+a = int(input("Введите число a "))
+b = int(input("Введите число b "))
+
+print(f"Сумма чисел a и b равна {folding(a, b)}")
